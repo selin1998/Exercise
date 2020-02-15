a55095b7569b1ed00d99619a395d3640fb127fe6
selinhabiboghlu <shebipoghlu2019@ada.edu.az>
2020-02-15 10:30:18 +0400

    methods are added

--- src/OddEven.java
+++ src/OddEven.java
@@ -1,51 +1,62 @@
 public class OddEven {
 
-    public static void main(String[] args) {
-        int n=10;int j;
+    public static void combine(int [] odds, int[] evens ){
 
-        int [] even=new int[n];
-        int [] odd=new int[n];
-        int [] mixed= new int[n*2];
-
-        for(int i=0;i<n;i++){
-            int x=(int)(Math.random()*100);
-            if(x%2==0){
-
-                even[i]= x;
-
-            }
-            else{
-                even[i]=x+1;
-            }
-
-
-        }
-
-        for(int i=0;i<n;i++) {
-            int x=(int)(Math.random()*100);
-               if(x%2==1){
-
-                odd[i]=x;
-
-            }
-               else{
-                   odd[i]=x+1;
-               }
-
-
-        }
-
-        for(int i=0;i<n;i++){
-            mixed[i]=odd[i];
+        int [] mixed= new int[odds.length*2];
+        int j;
+        for(int i=0;i<odds.length;i++){
+            mixed[i]=odds[i];
             System.out.print(mixed[i]+" ");
             j=i+1;
-            mixed[j]=even[i];
+            mixed[j]=evens[i];
             System.out.print(mixed[j]+" ");
 
 
 
         }
 
+
+    }
+
+    public static void fill(int[] array, boolean oddOrNot){
+            int control=0;
+            if(oddOrNot){
+                control=1;
+            }
+
+            for(int i=0;i<array.length;i++){
+                int x=(int)(Math.random()*100);
+                if(x%2==control){
+
+                    array[i]= x;
+
+                }
+                else{
+                    array[i]=x+1;
+                }
+
+
+            }
+
+
+
+        }
+
+
+
+
+    public static void main(String[] args) {
+        int n=10;int j;
+
+        int [] even=new int[n];
+        int [] odd=new int[n];
+
+
+
+
+        fill(odd,true);
+        fill(even, false);
+        combine(odd,even);
 
 
 
